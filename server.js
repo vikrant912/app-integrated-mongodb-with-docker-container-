37765f077f175800d936b65f668e17a6ff488794
Vikrant Mohite <vikrant.mohite@aithonsolutions.com>
2026-05-27 16:35:03 +0530

    Initial commit2705

--- server.js
+++ server.js
@@ -1,42 +1,46 @@
-let express = require('express');
-let path = require('path');
-let fs = require('fs');
-let MongoClient = require('mongodb').MongoClient;
-let bodyParser = require('body-parser');
-let app = express();
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const { MongoClient } = require('mongodb');
+const bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, "index.html"));
-  });
+// ✅ MongoDB connection (Docker network)
+const mongoUrl =
+  "mongodb://admin:password@mongodb:27017/my-db?authSource=admin";
 
+const mongoClientOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const databaseName = "my-db";
+
+// ✅ Home route
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// ✅ Profile image
 app.get('/profile-picture', function (req, res) {
   let img = fs.readFileSync(path.join(__dirname, "images/profile-1.jpg"));
-  res.writeHead(200, {'Content-Type': 'image/jpg' });
+  res.writeHead(200, { 'Content-Type': 'image/jpg' });
   res.end(img, 'binary');
 });
 
-// use when starting application locally
-let mongoUrlLocal = "mongodb://root:root@localhost:27017";
-
-// use when starting application as docker container
-let mongoUrlDocker = "mongodb://root:root@mongodb";
-
-// pass these options to mongo client connect request to avoid DeprecationWarning for current Server Discovery and Monitoring engine
-let mongoClientOptions = { useNewUrlParser: true, useUnifiedTopology: true };
-
-// "user-account" in demo with docker. "my-db" in demo with docker-compose
-let databaseName = "user-account";
-
+// ✅ Update profile
 app.post('/update-profile', function (req, res) {
   let userObj = req.body;
 
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
+  MongoClient.connect(mongoUrl, mongoClientOptions, function (err, client) {
+    if (err) {
+      console.error("Mongo connection error:", err);
+      return res.status(500).send("DB connection failed");
+    }
 
     let db = client.db(databaseName);
     userObj['userid'] = 1;
@@ -44,37 +48,48 @@ app.post('/update-profile', function (req, res) {
     let myquery = { userid: 1 };
     let newvalues = { $set: userObj };
 
-    db.collection("users").updateOne(myquery, newvalues, {upsert: true}, function(err, res) {
-      if (err) throw err;
-      client.close();
-    });
+    db.collection("users").updateOne(
+      myquery,
+      newvalues,
+      { upsert: true },
+      function (err) {
+        client.close();
+        if (err) {
+          console.error(err);
+          return res.status(500).send("Update failed");
+        }
 
+        res.send(userObj);
+      }
+    );
   });
-  // Send response
-  res.send(userObj);
 });
 
+// ✅ Get profile
 app.get('/get-profile', function (req, res) {
-  let response = {};
-  // Connect to the db
-  MongoClient.connect(mongoUrlLocal, mongoClientOptions, function (err, client) {
-    if (err) throw err;
+  MongoClient.connect(mongoUrl, mongoClientOptions, function (err, client) {
+    if (err) {
+      console.error("Mongo connection error:", err);
+      return res.status(500).send("DB connection failed");
+    }
 
     let db = client.db(databaseName);
-
     let myquery = { userid: 1 };
 
     db.collection("users").findOne(myquery, function (err, result) {
-      if (err) throw err;
-      response = result;
       client.close();
 
-      // Send response
-      res.send(response ? response : {});
+      if (err) {
+        console.error(err);
+        return res.status(500).send("Fetch failed");
+      }
+
+      res.send(result ? result : {});
     });
   });
 });
 
+// ✅ Start server
 app.listen(3000, function () {
-  console.log("app listening on port 3000!");
+  console.log("App listening on port 3000!");
 });
